96e3729ea14485bcd61de8bbf04d50deb6c51d9e
XPerianer <dominik.meier@student.hpi.de>
2021-02-20 11:09:02 +0100

    Refactor tests

--- tests/test_immediate.py
+++ tests/test_immediate.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 from unittest import mock
 
-
-def test_reordering(testdir):
+def prepare_testdir(testdir):
     testdir.makepyfile("""
         import pytest
 
@@ -13,6 +12,9 @@ def test_reordering(testdir):
             print("Two")
     """)
 
+
+def test_reordering(testdir):
+    prepare_testdir(testdir)
     result = testdir.runpytest(
         '-v',
         '--test-ordering=["test_reordering.py::test_one",'
@@ -38,17 +40,8 @@ def test_reordering(testdir):
 
 
 def test_socket_send(testdir):
+    prepare_testdir(testdir)
     with mock.patch("socketio.Client") as mock_class:
-        testdir.makepyfile("""
-            import pytest
-
-            def test_one():
-                print("One")
-
-            def test_two():
-                print("Two")
-        """)
-
         testdir.runpytest()
         assert mock_class.mock_calls == []
 
@@ -58,12 +51,18 @@ def test_socket_send(testdir):
         assert mock_class.mock_calls == [
             mock.call(),
             mock.call().connect('ws://localhost:9001'),
-            mock.call().emit('testreport', {'id': 'test_socket_send.py::test_one', 'when': 'setup', 'outcome': True}),
-            mock.call().emit('testreport', {'id': 'test_socket_send.py::test_one', 'when': 'call', 'outcome': True}),
-            mock.call().emit('testreport', {'id': 'test_socket_send.py::test_one', 'when': 'teardown', 'outcome': True}),
-            mock.call().emit('testreport', {'id': 'test_socket_send.py::test_two', 'when': 'setup', 'outcome': True}),
-            mock.call().emit('testreport', {'id': 'test_socket_send.py::test_two', 'when': 'call', 'outcome': True}),
-            mock.call().emit('testreport', {'id': 'test_socket_send.py::test_two', 'when': 'teardown', 'outcome': True}),
+            mock.call().emit('testreport',
+                {'id': 'test_socket_send.py::test_one', 'when': 'setup', 'outcome': True}),
+            mock.call().emit('testreport',
+                {'id': 'test_socket_send.py::test_one', 'when': 'call', 'outcome': True}),
+            mock.call().emit('testreport',
+                {'id': 'test_socket_send.py::test_one', 'when': 'teardown', 'outcome': True}),
+            mock.call().emit('testreport',
+                {'id': 'test_socket_send.py::test_two', 'when': 'setup', 'outcome': True}),
+            mock.call().emit('testreport',
+                {'id': 'test_socket_send.py::test_two', 'when': 'call', 'outcome': True}),
+            mock.call().emit('testreport',
+                {'id': 'test_socket_send.py::test_two', 'when': 'teardown', 'outcome': True}),
             mock.call().__bool__(),
             mock.call().disconnect(),
             mock.call().wait(),
